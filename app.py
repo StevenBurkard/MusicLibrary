8380d9b8ae512288412e4e04fe0400d5267c2b09
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-06-21 09:29:38 -0500

    Updated app.py, added a method to get all the data in the song table

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from marshmallow import post_load, fields, ValidationError
 from dotenv import load_dotenv
 from os import environ
 
@@ -23,15 +24,32 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    artist = db.Column(db.String(255), nullable=False)
+    album = db.Column(db.String(255), nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+    genre = db.Column(db.String(255), nullable=False)
 
+    def __repr__(self):
+        return f'{self.title} {self.artist} {self.album} {self.release_date} {self.genre}'
 
 
 # Schemas
-
-
+class SongSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "artist", "album", "release_date", "genre")
+    
+song_schema = SongSchema()
+songs_schema = SongSchema(many=True)
 
 # Resources
-
+class SongListResource(Resource):
+    def get(self):
+        all_songs = Song.query.all()
+        return songs_schema.dump(all_songs)
 
 
 # Routes
+api.add_resource(SongListResource, '/api/songs')
